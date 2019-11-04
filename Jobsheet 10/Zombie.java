b003e53896a564a49f207d8d703e03a684f87c9a
raihanrach <54708783+raihanrach@users.noreply.github.com>
2019-11-04 17:44:18 +0700

    Add files via upload
    Assalamualaikum ini tugas jobsheet 10

---
+++ Jobsheet 10/Zombie.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package polimorfisme;
+
+/**
+ *
+ * @author hp
+ */
+public class Zombie {
+   protected int health;
+    protected int level;
+    
+    public void heal(){
+        
+    }
+
+   
+    public void destroyed() {
+    
+    }
+    
+    public String getZombieInfo(){
+        return "n";
+        
+    }
+}
