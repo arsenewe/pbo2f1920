22cb8a324b0f31871bf5b59b96601f5a57fe4895
HaninSyafiqRoziqie <haninsyafiq@gmail.com>
2019-11-04 17:40:32 +0700

    04-11-19

---
+++ HaninSyafiqRoziqie/JS10/src/tugaspolimorfisme/Barrier.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugaspolimorfisme;
+
+/**
+ *
+ * @author ROG
+ */
+public class Barrier implements Destroyable {
+    private int strength;
+    
+    public Barrier (int strength){
+        this.strength = strength;
+    }
+    public void setStrength (int strength){
+        this.strength = strength;
+    }
+    public int getStrength (){
+        return strength;
+    } 
+    public void destroy(){
+        strength -= strength*0.1;
+    }  
+    public void destroyed(){
+        destroy();
+    }
+    public String getBarrierInfo(){
+        return "Barrier Strength = "+strength+"\n";
+    }
+}
