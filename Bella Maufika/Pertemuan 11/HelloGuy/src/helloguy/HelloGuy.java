519f3b76528cc5efd099ac8a2bf0843e3d748f13
bellaarina <bellaarina11@gmail.com>
2019-11-06 16:33:38 +0700

    'TUGASGUI11'

---
+++ Bella Maufika/Pertemuan 11/HelloGuy/src/helloguy/HelloGuy.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloguy;
+
+/**
+ *
+ * @author bella
+ */
+public class HelloGuy {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
