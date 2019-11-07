481963f18b62ec59b3c20f6f3e29c7813022fcfb
Andhika Utama <andhikautamaa04@gmail.com>
2019-11-07 10:29:58 +0800

    Jobsheet 10 dan 11

---
+++ Andhika Utama/Pertemuan 10/polimorfisme/src/polimorfisme/Plant.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package polimorfisme;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Plant {
+
+    void doDestroy(WalkingZombie wz) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void doDestroy(JumpingZombie jz) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
