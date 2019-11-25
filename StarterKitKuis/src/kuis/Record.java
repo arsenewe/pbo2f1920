63efca0ed77d0b9f06b833d1ebcbad2296588fe9
arsenewenggar <enggar@polinema.ac.id>
2019-11-25 09:30:52 +0700

    add starter kit kuis

---
+++ StarterKitKuis/src/kuis/Record.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kuis;
+
+/**
+ *
+ * @author septianenggarsukmana
+ */
+public class Record {
+    private String angka1, angka2, operator, hasil;
+    
+    public Record(){
+        
+    }
+    
+}
