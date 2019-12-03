0cd96ef8f7f756a87a181b12c3a858c77be41351
anggistaamaliayashinta <57085776+anggistaamaliayashinta@users.noreply.github.com>
2019-12-03 10:42:01 +0700

    Add files via upload

---
+++ AnggistaAmaliaYashinta07/LearnUnitTesting/test/unittest/MessageProcessorTest.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package unittest;
+
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ *
+ * @author hokyt
+ */
+public class MessageProcessorTest {
+    MessageProcessor mProcessor;
+    
+    public MessageProcessorTest() {
+        mProcessor = new MessageProcessor();
+        mProcessor.setSender("Ronaldo");
+        mProcessor.setRecipient("Rafael");
+        mProcessor.setMessage("Whats up bro?");
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+    
+    @Test
+    public void testShowMessage(){
+        String expectedResult = "Hai Rafael, you have message from Ronaldo."
+                + "\nThe message as follows : Whats up bro?";
+        Assert.assertEquals(expectedResult, mProcessor.messageFormat());
+    }
+}
